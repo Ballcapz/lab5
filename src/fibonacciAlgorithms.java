e64e9393080ea395e3649e6236d35947ae323646
Zach Johnson <zlg23johnson@gmail.com>
2019-10-19 12:18:21 -0500

    Finished testing all functions and some clean up.

--- src/fibonacciAlgorithms.java
+++ src/fibonacciAlgorithms.java
@@ -18,14 +18,14 @@ public class fibonacciAlgorithms {
     static long MAXVALUE =  2000000000;
     static long MINVALUE = -2000000000;
     private static int numberOfTrials = 1;
-    private static int MAXINPUTSIZE  = (int) Math.pow(2, 26);
+    private static int MAXINPUTSIZE  = 50;
     private static int MININPUTSIZE  =  1;
 //    private static int SIZEINCREMENT = 2;
 
     static Random rand = new Random();
 
     // static int SIZEINCREMENT =  10000000; // not using this since we are doubling the size each time
-    private static String ResultsFolderPath = "/home/zach/Results/lab4/"; // pathname to results folder
+    private static String ResultsFolderPath = "/home/zach/Results/lab5/"; // pathname to results folder
     private static FileWriter resultsFile;
     private static PrintWriter resultsWriter;
 
@@ -34,10 +34,25 @@ public class fibonacciAlgorithms {
     public static void main(String args[])
     {
         verifyCorrectFibonacciNumber();
-//        runFullExperiment("slowQuickSort-ONSORTED-1-TRASH.txt");
-//        runFullExperiment("slowQuickSort-ONSORTED-2.txt");
-//        runFullExperiment("slowQuickSort-ONSORTED-3.txt");
+//        runFullExperiment("fibRecur-1-TRASH.txt");
+//        runFullExperiment("fibRecur-2.txt");
+//        runFullExperiment("fibRecur-3.txt");
     }
+
+    // verification function that makes sure we get the correct fib number
+    // Basically a unit test by hand
+    private static void verifyCorrectFibonacciNumber() {
+        int expected = 4181;
+        long result = fibLoop(19);
+        System.out.println("Fib number is: " + result);
+        if (result == expected) {
+            System.out.println("Fib number is correct");
+        } else {
+            System.out.println("BROKEN !!!!!!");
+        }
+    }
+
+
 
     private static void runFullExperiment(String resultsFileName) {
         try {
@@ -54,13 +69,13 @@ public class fibonacciAlgorithms {
         ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
 
 
-        resultsWriter.println("#InputSize    AverageTime        DoublingRatio"); // # marks a comment in gnuplot data
+        resultsWriter.println("#X(Value)    N(InputSize)   AverageTime        Fib Number"); // # marks a comment in gnuplot data
 
         resultsWriter.flush();
 
         /* for each size of input we want to test: in this case starting small and doubling the size each time */
 //        double[] timeRatios;
-        double previousTime = 0;
+//        double previousTime = 0;
 
         for(int inputSize=MININPUTSIZE;inputSize<=MAXINPUTSIZE; inputSize++) {
 
@@ -74,7 +89,7 @@ public class fibonacciAlgorithms {
 
             /* force garbage collection before each batch of trials run so it is not included in the time */
 
-            System.out.println("Collecting the trash...");
+//            System.out.println("Collecting the trash...");
             System.gc();
 
             // instead of timing each individual trial, we will time the entire set of trials (for a given input size)
@@ -85,15 +100,15 @@ public class fibonacciAlgorithms {
 
             //BatchStopwatch.start(); // comment this line if timing trials individually
 
-
+            long result = 0;
 
             // run the trials
-            System.out.println("Timing Each sort individually wo gc every time forced...");
+//            System.out.println("Timing Each sort individually wo gc every time forced...");
             System.out.print("    Starting trials for input size "+inputSize+" ... ");
             for (long trial = 0; trial < numberOfTrials; trial++) {
 
 
-                long[] testList = createAscendingList(inputSize);
+//                long[] testList = createAscendingList(inputSize);
 
                 /* force garbage collection before each trial run so it is not included in the time */
                 //System.gc();
@@ -110,7 +125,7 @@ public class fibonacciAlgorithms {
                 /*              DO BIDNESS              */
                 /////////////////////////////////////////
 
-
+                result = fibRecur(inputSize);
 
 
                 ///////////////////////////////////////////
@@ -124,15 +139,15 @@ public class fibonacciAlgorithms {
             //batchElapsedTime = BatchStopwatch.elapsedTime(); // *** comment this line if timing trials individually
 
             double averageTimePerTrialInBatch = (double) batchElapsedTime / (double)numberOfTrials; // calculate the average time per trial in this batch
-            double doublingRatio = 0;
-            if (previousTime > 0) {
-                doublingRatio = averageTimePerTrialInBatch / previousTime;
-            }
-
-            previousTime = averageTimePerTrialInBatch;
+//            double doublingRatio = 0;
+//            if (previousTime > 0) {
+//                doublingRatio = averageTimePerTrialInBatch / previousTime;
+//            }
+//
+//            previousTime = averageTimePerTrialInBatch;
             /* print data for this size of input */
 
-            resultsWriter.printf("%12d  %18.2f %18.1f\n",inputSize, averageTimePerTrialInBatch, doublingRatio); // might as well make the columns look nice
+            resultsWriter.printf("%10d %12d  %18.2f %18d\n",inputSize, log2(inputSize), averageTimePerTrialInBatch, result); // might as well make the columns look nice
 
             resultsWriter.flush();
 
@@ -203,7 +218,7 @@ public class fibonacciAlgorithms {
         long res = fibRecurDPWorker(x - 1, memo) + fibRecurDPWorker(x - 2, memo);
 
         // store added result in the array for the future, where it is passed recursively to the
-        // function
+        // function so it doesn't have to be recomputed
         memo[x] = res;
 
         // return the result to be used in the next recursive call
@@ -222,7 +237,7 @@ public class fibonacciAlgorithms {
         if (x == 0)
             return 0;
         // the function to put the matrix to the power of x
-        power(F, x - 1);
+        fibMatrixWorker(F, x - 1);
         // return the result
         return F[0][0];
     }
@@ -247,10 +262,10 @@ public class fibonacciAlgorithms {
     }
 
     /*
-      Helper function that calculates F[][] raise to the power n and puts the
+      The actual worker function that calculates F[][] raise to the power n and puts the
       result in F[][]
      */
-    static void power(long F[][], long x) {
+    static void fibMatrixWorker(long F[][], long x) {
         long i;
         // new matrix of the same value to use for our multiplication
         long M[][] = new long[][] { { 1, 1 }, { 1, 0 } };
@@ -263,19 +278,9 @@ public class fibonacciAlgorithms {
 
 
 
-    
 
 
-    private static void verifyCorrectFibonacciNumber() {
-        int expected = 4181;
-        long result = fibMatrix(19);
-        System.out.println("Fib number is: " + result);
-        if (result == expected) {
-            System.out.println("Fib number is correct");
-        } else {
-            System.out.println("BROKEN !!!!!!");
-        }
-    }
+
 
 
     //<editor-fold desc="Utilities">
